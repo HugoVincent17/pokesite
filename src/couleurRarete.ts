1677a6a2b8a84a19c6e9b3d7cab1d74bbb9f4f34
Hugo <stage2@eixa6.com>
2025-12-17 11:32:43 +0100

    ajout fichier pour la couleur des pokémons par rapport à la rareté

---
+++ src/couleurRarete.ts
@@ -0,0 +1,7 @@
+export const couleurRarete: { [key: string]: string } = {
+    Légendaire: "#FFBF2A",     // vrai or
+    Fabuleux: "#C0C0C0",       // silver mystique
+    Fossile: "#D2B48C",        // marron clair
+    Surpuissant: "#FF7500",    // orange “jeune”, moins saturé
+    Starter: "#FFBC99"          // rouge moins fort
+  };
