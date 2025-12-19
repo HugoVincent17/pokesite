f2e73f736625c4b54f0690d53ce15dd18d759fd8
Hugo <stage2@eixa6.com>
2025-12-19 11:12:38 +0100

    regroupement de tous les paramètres dans un seul fichier

---
+++ src/parametres.ts
@@ -0,0 +1,28 @@
+export const couleurRarete: { [key: string]: string } = {
+    Légendaire: "#FFBF2A",     // vrai or
+    Fabuleux: "#C0C0C0",       // silver mystique
+    Surpuissant: "#FF7500",    // orange “jeune”, moins saturé
+    Starter: "#FFBC99",          // rouge moins fort
+    Fossile: "#D2B48C"        // marron clair
+  };
+
+  export const couleurType: { [key: string]: string } = {
+    Plante: "#78C850",
+    Feu: "#F08030",
+    Eau: "#6890F0",
+    Électrik: "#F8D030",
+    Glace: "#98D8D8",
+    Combat: "#C03028",
+    Poison: "#A040A0",
+    Sol: "#E0C068",
+    Vol: "#A890F0",
+    Psy: "#F85888",
+    Insecte: "#A8B820",
+    Roche: "#B8A038",
+    Spectre: "#705898",
+    Dragon: "#7038F8",
+    Ténèbres: "#705848",
+    Acier: "#B8B8D0",
+    Fée: "#EE99AC",
+    Normal: "#A8A878",
+  };
