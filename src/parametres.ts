1c224cc617325a32e285103af1c48e78b94199fc
HugoVincent17 <hugovincent6717@gmail.com>
2026-05-07 15:05:19 +0200

    ajout table des types

--- src/parametres.ts
+++ src/parametres.ts
@@ -29,3 +29,24 @@ export const couleurRarete: { [key: string]: string } = {
     Normal: "#FFFFFF",
   };
 
+// 2 = Super efficace, 0.5 = Pas très efficace, 0 = Immunité
+export const tableTypes: { [key: string]: { [key: string]: number } } = {
+  Acier: { Acier: 0.5, Eau: 0.5, Électrik: 0.5, Fée: 2, Feu: 0.5, Glace: 2, Roche: 2 },
+  Combat: { Acier: 2, Glace: 2, Insecte: 0.5, Normal: 2, Poison: 0.5, Psy: 0.5, Roche: 2, Spectre: 0, Ténèbres: 2, Vol: 0.5, Fée: 0.5 },
+  Dragon: { Acier: 0.5, Dragon: 2, Fée: 0 },
+  Eau: { Dragon: 0.5, Eau: 0.5, Feu: 2, Plante: 0.5, Roche: 2, Sol: 2 },
+  Électrik: { Dragon: 0.5, Eau: 2, Électrik: 0.5, Plante: 0.5, Sol: 0, Vol: 2 },
+  Fée: { Combat: 2, Dragon: 2, Feu: 0.5, Poison: 0.5, Ténèbres: 2, Acier: 0.5 },
+  Feu: { Acier: 2, Dragon: 0.5, Eau: 0.5, Feu: 0.5, Glace: 2, Insecte: 2, Plante: 2, Roche: 0.5 },
+  Glace: { Acier: 0.5, Dragon: 2, Eau: 0.5, Feu: 0.5, Glace: 0.5, Plante: 2, Sol: 2, Vol: 2 },
+  Insecte: { Combat: 0.5, Feu: 0.5, Poison: 0.5, Spectre: 0.5, Vol: 0.5, Acier: 0.5, Fée: 0.5, Plante: 2, Psy: 2, Ténèbres: 2 },
+  Normal: { Acier: 0.5, Roche: 0.5, Spectre: 0 },
+  Plante: { Acier: 0.5, Dragon: 0.5, Eau: 2, Feu: 0.5, Insecte: 0.5, Plante: 0.5, Poison: 0.5, Roche: 2, Sol: 2, Vol: 0.5 },
+  Poison: { Plante: 2, Poison: 0.5, Roche: 0.5, Sol: 0.5, Spectre: 0.5, Acier: 0, Fée: 2 },
+  Psy: { Combat: 2, Poison: 2, Psy: 0.5, Acier: 0.5, Ténèbres: 0 },
+  Roche: { Acier: 0.5, Combat: 0.5, Feu: 2, Glace: 2, Insecte: 2, Sol: 0.5, Vol: 2 },
+  Sol: { Acier: 2, Électrik: 2, Feu: 2, Insecte: 0.5, Plante: 0.5, Poison: 2, Roche: 2, Vol: 0 },
+  Spectre: { Normal: 0, Psy: 2, Spectre: 2, Ténèbres: 0.5 },
+  Ténèbres: { Combat: 0.5, Psy: 2, Spectre: 2, Ténèbres: 0.5, Fée: 0.5 },
+  Vol: { Combat: 2, Électrik: 0.5, Insecte: 2, Plante: 2, Roche: 0.5, Acier: 0.5 },
+};
