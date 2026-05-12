bd7eb9e5394988957d866af2c6c9c824630ced9e
HugoVincent17 <hugovincent6717@gmail.com>
2026-05-12 16:49:35 +0200

    début ajout routes auth

--- pokesite_api/routes/api.php
+++ pokesite_api/routes/api.php
@@ -1,11 +1,21 @@
 <?php 
 use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 // Route racine de l'API
 Route::get('/', function () {
     return "API Pokédex en ligne";
 });
+
+/*Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});*/
 
 // Routes Pokemon
 Route::get('/pokemon', [PokemonController::class, 'index']);
@@ -23,3 +33,5 @@ Route::get('/pokemon/error-trigger', function () {
 Route::get('/debug-server-error', function () {
     return response()->json(['error' => 'Erreur serveur'], 500);
 });
+
+Route::post('/logout', [AuthController::class, 'logout']);
