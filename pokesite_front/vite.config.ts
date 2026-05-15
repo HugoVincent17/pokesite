51279b88e049da7cd13932038fddb5bf1fb8927a
HugoVincent17 <hugovincent6717@gmail.com>
2026-05-15 12:57:47 +0200

    ajout vitest pour les test

--- pokesite_front/vite.config.ts
+++ pokesite_front/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom', // Simule un navigateur
+  }
 })
