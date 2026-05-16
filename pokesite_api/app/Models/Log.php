827786cf14417a38636b34f0678b42d96e9df132
HugoVincent17 <hugovincent6717@gmail.com>
2026-05-16 10:36:11 +0200

    commentaires de documentation

--- pokesite_api/app/Models/Log.php
+++ pokesite_api/app/Models/Log.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * MODÈLE LOG
- * 
- * Ce modèle représente la table 'logs' en base de données.
- * Il est utilisé pour assurer le suivi des actions utilisateurs
- */
+* MODÈLE LOG
+* 
+* Ce modèle représente la table 'logs' en base de données.
+* Il est utilisé pour assurer le suivi des actions utilisateurs
+*/
 
 class Log extends Model
 {
@@ -18,14 +18,14 @@ class Log extends Model
     use HasFactory;
 
     /**
-     * PROTECTION CONTRE L'ASSIGNATION DE MASSE (MASS ASSIGNMENT). L'assignation de masse c'est 
-     * lorsqu'on utilise des méthodes comme Log::create() ou $log->fill() pour remplir les champs d'un modèle à partir d'un tableau de données.
-     * 
-     * Sécurité : Seuls les champs listés ci-dessous peuvent être remplis via 
-     * les méthodes Log::create() ou $log->fill().
-     * Cela empêche un utilisateur malveillant d'injecter des données dans des colonnes
-     * sensibles non autorisées
-     */
+    * PROTECTION CONTRE L'ASSIGNATION DE MASSE (MASS ASSIGNMENT). L'assignation de masse c'est 
+    * lorsqu'on utilise des méthodes comme Log::create() ou $log->fill() pour remplir les champs d'un modèle à partir d'un tableau de données.
+    * 
+    * Sécurité : Seuls les champs listés ci-dessous peuvent être remplis via 
+    * les méthodes Log::create() ou $log->fill().
+    * Cela empêche un utilisateur malveillant d'injecter des données dans des colonnes
+    * sensibles non autorisées
+    */
 
     protected $fillable = ['user_name', 'action'];
 }
